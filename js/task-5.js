52317b0873077694aeaba1c6d1722044dfd5e942
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-08-07 13:57:15 +0200

    add commit

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,12 @@
+const body = document.body;
+const colorSpan = document.querySelector(".color");
+const changeColorButton = document.querySelector(".change-color");
+changeColorButton.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  colorSpan.textContent = newColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
