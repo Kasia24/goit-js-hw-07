73604977ba66d9d36651411b7c36c027da137cfe
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-08-07 14:01:56 +0200

    add commit

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,38 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  const boxesContainer = document.getElementById("boxes");
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    fragment.appendChild(box);
+  }
+
+  boxesContainer.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  const boxesContainer = document.getElementById("boxes");
+  boxesContainer.innerHTML = "";
+}
+
+document.querySelector("[data-create]").addEventListener("click", () => {
+  const input = document.querySelector("#controls input");
+  const amount = parseInt(input.value, 10);
+
+  if (amount >= 1 && amount <= 100) {
+    destroyBoxes();
+    createBoxes(amount);
+    input.value = "";
+  }
+});
+
+document.querySelector("[data-destroy]").addEventListener("click", () => {
+  destroyBoxes();
+});
